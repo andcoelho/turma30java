abd6edfbe9335448e064b258b3f2a5ebceff8c27
Anderson <andcoelho97@gmail.com>
2021-08-04 10:57:54 -0300

    Exemplos laço while e do while

---
+++ java/AULAS/src/TesteDoWhile.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TesteDoWhile {
+	
+	public static void main(String[] args) {
+		
+Scanner scan = new Scanner(System.in);
+		
+		//Variáveis
+		String nome = "", nomeMaiorSalario = "";
+		int salario = 0, maiorSalario = 0;
+		
+		//Entrada
+		
+		do{
+			if (salario > maiorSalario) {
+				maiorSalario = salario;
+				nomeMaiorSalario = nome;
+			}
+			System.out.printf("Digite o nome: ");
+			nome = scan.next();
+			System.out.print("Digite o seu salário: ");
+			salario = scan.nextInt();
+			
+		}while(salario > 0);
+		
+		System.out.printf("O maior salario é %d da pessoa %s", maiorSalario, nomeMaiorSalario);
+		
+	}
+}
---
+++ java/AULAS/src/TesteEnquanto.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class TesteEnquanto {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		//Variáveis
+		String nome = "", nomeMaiorSalario = "";
+		int salario = 0, maiorSalario = 0;
+		
+		//Entrada
+		
+		System.out.printf("Digite o nome: ");
+		nome = scan.next();
+		System.out.print("Digite o seu salário: ");
+		salario = scan.nextInt();
+		
+		while(salario > 0) {
+			if (salario > maiorSalario) {
+				maiorSalario = salario;
+				nomeMaiorSalario = nome;
+			}
+			System.out.printf("Digite o nome: ");
+			nome = scan.next();
+			System.out.print("Digite o seu salário: ");
+			salario = scan.nextInt();
+			
+		}
+		
+		System.out.printf("O maior salario é %d da pessoa %s", maiorSalario, nomeMaiorSalario);		
+		
+		scan.close();
+		
+	}
+}
