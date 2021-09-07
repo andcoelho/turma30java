95140290975a2b55e42ae6fc1b47182c29129473
Anderson <andcoelho97@gmail.com>
2021-09-07 11:26:22 -0300

    farmacia api - repository

---
+++ farmacia/src/main/java/com/farmaciageneration/farmacia/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.farmaciageneration.farmacia.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.farmaciageneration.farmacia.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+
+}
